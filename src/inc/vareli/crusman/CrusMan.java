2272b56519e5c7b38cacd60f7f259366e2dd8b02
Sebastien <sebastien.peterson.boudreau@gmail.com>
2023-11-01 08:46:02 +0000

    Laid groundwork for basic data classes. About 80% done :)

---
+++ src/inc/vareli/crusman/CrusMan.java
@@ -0,0 +1,13 @@
+package inc.vareli.crusman;
+
+public class CrusMan {
+	public static void main(String[] args) {
+			Ship ship;
+			Trip trip;
+			//this is the class that launches the GUI for the user to
+			//interface with the system.
+			//in the end this will be in a different package from the other 
+			//classes, but for now they're all packaged into a single package
+			//until we know the scope better
+	}
+}
