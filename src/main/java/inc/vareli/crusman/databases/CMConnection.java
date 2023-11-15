c3432200ca052fb4f5aa5bf01eab00710c8bcbea
Sebastien <sebastien.peterson.boudreau@gmail.com>
2023-11-15 14:52:43 -0400

    updated databases foundation to fit the interaces i will be using in the other branches

---
+++ src/main/java/inc/vareli/crusman/databases/CMConnection.java
@@ -0,0 +1,15 @@
+package inc.vareli.crusman.databases;
+
+import java.sql.*;
+
+public class CMConnection implements Connection {
+	/*
+	 * Arhaan, I put the name and stuff here as a starting point, but you don't 
+	 * do it one particular way. The rest of the program will just be written 
+	 * so that it requires a Connection instance, so if you write a class that
+	 * implements Connection, or a class that provides a Connection by returning
+	 * it etc, that's fine.
+	 * I'm just going to write them to use the java.sql.Connection interface.
+	 *
+	 */
+}
