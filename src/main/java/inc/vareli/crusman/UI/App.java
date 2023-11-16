daac96a67251054ba7369503fddd4d75c62559a6
sebastien <sebastien.peterson.boudreau@gmail.com>
2023-11-15 23:58:31 -0400

    changed some formatting

--- src/main/java/inc/vareli/crusman/UI/App.java
+++ src/main/java/inc/vareli/crusman/UI/App.java
@@ -1,7 +1,7 @@
 package inc.vareli.crusman.UI;
 
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
