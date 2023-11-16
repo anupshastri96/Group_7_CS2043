d0a85e585c897975b1890037426f9fc0de00e611
MartCPal <martc.palamine@unb.ca>
2023-11-16 01:42:12 -0400

    added next & prev buttons

--- cruiseShipGUI.java
+++ cruiseShipGUI.java
@@ -1,11 +1,6 @@
 /**
  Rough GUI template/layout for Cruise ship application
-
- @author Mart Cesar Palamine
- CS2043 
-
- cruiseShipGUI.java
- 
+ @author Mart Cesar Palamine        CS2043
  */
 
 import javafx.application.Application;
@@ -15,12 +10,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
-import javafx.scene.layout.FlowPane;
 import javafx.geometry.Pos;
 import javafx.event.ActionEvent; 
 import java.text.NumberFormat;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 
 public class cruiseShipGUI extends Application {
     private Text shipInfoBox1;
@@ -30,13 +26,10 @@ public class cruiseShipGUI extends Application {
     public void start (Stage primaryStage) {
 
         BorderPane root = new BorderPane();
+        AnchorPane anchorPane = new AnchorPane();
         VBox vbox = new VBox(85);
-        VBox vbox2 = new VBox(50);
-
-        
-
-        //vbox.setAlignment(Pos.BASELINE_RIGHT);
-       // vbox2.setAlignment(Pos.CENTER);
+        VBox vbox2 = new VBox(95);
+        HBox hbox = new HBox(350);
 
         Button butt1 = new Button("BOOK");
         butt1.setPrefWidth(75);
@@ -58,27 +51,17 @@ public class cruiseShipGUI extends Application {
         prev.setPrefWidth(75);
         prev.setOnAction(this::prevAction);
 
-        
-
         shipInfoBox1 = new Text("Ship info here");
         shipInfoBox2 = new Text("Ship info here");
         shipInfoBox3 = new Text("Ship info here");
 
         root.setCenter(vbox2);
         root.setRight(vbox);
-
+        root.setBottom(hbox);
+        
+        hbox.getChildren().addAll(next, prev);
         vbox2.getChildren().addAll(shipInfoBox1, shipInfoBox2, shipInfoBox3);
         vbox.getChildren().addAll(butt1, butt2, butt3);
-
-        /** 
-        FlowPane pane = new FlowPane
-            (appName, shipInfoBox1, shipInfoBox2, shipInfoBox3,butt1, butt2,
-            butt3, next, prev);
-
-            pane.setAlignment(Pos.CENTER);
-            pane.setHgap(50);
-            pane.setVgap(50);
-        */
 
         Scene scene = new Scene (root, 500, 500);
 
@@ -86,16 +69,8 @@ public class cruiseShipGUI extends Application {
         primaryStage.setTitle("Cruise Ship Application");
         primaryStage.show(); 
 
-        
-
     }
-
-        public void bookAction (ActionEvent event) {
-        }
-
-        public void nextAction (ActionEvent event) {
-        }
-
-        public void prevAction (ActionEvent event) {
-        }
+        public void bookAction (ActionEvent event) {}
+        public void nextAction (ActionEvent event) {}
+        public void prevAction (ActionEvent event) {}
 }
