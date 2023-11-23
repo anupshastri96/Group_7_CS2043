abae5b5f5280675e86d16faaf71e5c46a9c4d750
Sebastien <sebastien.peterson.boudreau@unb.ca>
2023-11-23 14:33:19 -0400

    trying the array now

--- src/main/java/inc/vareli/crusman/UI/CruiseShipGUI.java
+++ src/main/java/inc/vareli/crusman/UI/CruiseShipGUI.java
@@ -126,8 +126,8 @@ public class CruiseShipGUI extends Application {
         
 	//this too
         hbox.getChildren().addAll(next, prev);
-        vbox2.getChildren().addAll(tripListings[0]);
-        vbox.getChildren().addAll(bookingButtons[0]);
+        vbox2.getChildren().addAll(tripListings);
+        vbox.getChildren().addAll(bookingButtons);
 
         browsingScene = new Scene(root, 500, 500);
 
