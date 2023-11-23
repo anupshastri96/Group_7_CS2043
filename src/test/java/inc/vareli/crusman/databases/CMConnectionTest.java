e036f5e5cf4e110cd44154d660772ec258854a85
Arhaan Sami <63785911+arhaan-s@users.noreply.github.com>
2023-11-23 00:28:48 -0400

    Added functionality to create ships to add to the database, adn retrieve ships from the database

--- src/test/java/inc/vareli/crusman/databases/CMConnectionTest.java
+++ src/test/java/inc/vareli/crusman/databases/CMConnectionTest.java
@@ -2,7 +2,11 @@ package inc.vareli.crusman.databases;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.EnumMap;
+
 import org.junit.jupiter.api.Test;
+
+import inc.vareli.crusman.databases.Ship.RoomType;
 
 class CMConnectionTest {
 
@@ -10,6 +14,28 @@ class CMConnectionTest {
 	void testCMConnection() {
 		//create a database connection in my database
 		CMConnection testConnection = new CMConnection("jdbc:mysql://cs1103.cs.unb.ca:3306/j3zh5", "j3zh5", "rGR45WHX");
+	}
+	
+	@Test
+	void testCreateShip() {
+		CMConnection testConnection = new CMConnection("jdbc:mysql://cs1103.cs.unb.ca:3306/j3zh5", "j3zh5", "rGR45WHX");
+		EnumMap<RoomType,Integer> roomCounts = new EnumMap<RoomType,Integer>(RoomType.class);
+		roomCounts.put(RoomType.INTERIOR, 1);
+		roomCounts.put(RoomType.OUTSIDE, 1);
+		roomCounts.put(RoomType.BALCONY, 1);
+		roomCounts.put(RoomType.SUITE, 2);
+		
+		EnumMap<RoomType,Integer> roomCounts2 = new EnumMap<RoomType,Integer>(RoomType.class);
+		roomCounts2.put(RoomType.INTERIOR, 2);
+		roomCounts2.put(RoomType.OUTSIDE, 1);
+		roomCounts2.put(RoomType.BALCONY, 1);
+		roomCounts2.put(RoomType.SUITE, 2);
+		try {
+		Ship s = testConnection.createShip(roomCounts);
+		Ship s2 = testConnection.createShip(roomCounts2);
+		} catch(IllegalArgumentException e) {
+			System.out.println(e);
+		}
 	}
 
 }
