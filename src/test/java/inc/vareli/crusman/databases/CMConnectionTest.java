8377806eeefd33a78cec6245272763460ebd8693
sebastien <sebastien.peterson.boudreau@gmail.com>
2023-11-23 07:26:17 -0400

    Updated AppTest to remove reliance on dummy implementation of CMConnection. Updated CMConnection to fit the style guides better and to use abstract interfaces rather than concretions (ArrayList -> List)

--- src/test/java/inc/vareli/crusman/databases/CMConnectionTest.java
+++ src/test/java/inc/vareli/crusman/databases/CMConnectionTest.java
@@ -1,55 +1,62 @@
 package inc.vareli.crusman.databases;
 
-//import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.ArrayList;
-import java.util.EnumMap;
-
-//import org.junit.jupiter.api.Test;
-
 import inc.vareli.crusman.databases.Ship.RoomType;
 
-class CMConnectionTest {
+import org.junit.Test;
+import java.util.List;
+import java.util.EnumMap;
 
-	//@Test
-	void testCMConnection() {
-		//create a database connection in my database
-		CMConnection testConnection = new CMConnection("jdbc:mysql://cs1103.cs.unb.ca:3306/j3zh5", "j3zh5", "rGR45WHX");
+import static org.junit.Assert.assertTrue;
+
+public class CMConnectionTest {
+
+	@Test
+	public void testCMConnection() {
+		CMConnection testConnection = new CMConnection(
+										"jdbc:mysql://cs1103.cs.unb.ca:3306/j3zh5",
+										"j3zh5", "rGR45WHX");
+		assertTrue(testConnection != null);
 	}
 	
-	//@Test
-	void testCreateShip() {
-		CMConnection testConnection = new CMConnection("jdbc:mysql://cs1103.cs.unb.ca:3306/j3zh5", "j3zh5", "rGR45WHX");
-		EnumMap<RoomType,Integer> roomCounts = new EnumMap<RoomType,Integer>(RoomType.class);
+	@Test
+	public void testCreateShip() {
+		CMConnection testConnection = new CMConnection(
+										"jdbc:mysql://cs1103.cs.unb.ca:3306/j3zh5",
+										"j3zh5", "rGR45WHX");
+
+		EnumMap<RoomType,Integer> roomCounts = 
+								new EnumMap<RoomType,Integer>(RoomType.class);
 		roomCounts.put(RoomType.INTERIOR, 1);
 		roomCounts.put(RoomType.OUTSIDE, 1);
 		roomCounts.put(RoomType.BALCONY, 1);
 		roomCounts.put(RoomType.SUITE, 2);
 		
-		EnumMap<RoomType,Integer> roomCounts2 = new EnumMap<RoomType,Integer>(RoomType.class);
+		EnumMap<RoomType,Integer> roomCounts2 =
+								new EnumMap<RoomType,Integer>(RoomType.class);
 		roomCounts2.put(RoomType.INTERIOR, 2);
 		roomCounts2.put(RoomType.OUTSIDE, 1);
 		roomCounts2.put(RoomType.BALCONY, 1);
 		roomCounts2.put(RoomType.SUITE, 2);
+
 		try {
-		Ship s = testConnection.createShip(roomCounts);
-		Ship s2 = testConnection.createShip(roomCounts2);
+			Ship s = testConnection.createShip(roomCounts);
+			Ship s2 = testConnection.createShip(roomCounts2);
+			assertTrue(s != null && s2 != null);
 		} catch(IllegalArgumentException e) {
 			System.out.println(e);
 		}
 	}
-	
-	//@Test
-	void testQueryShip() {
+
+	@Test
+	public void testQueryShip() {
 		try{
 			CMConnection testConnection = new CMConnection("jdbc:mysql://cs1103.cs.unb.ca:3306/j3zh5", "j3zh5", "rGR45WHX");
-			ArrayList<Ship> shipList = testConnection.queryShip();
+			List<Ship> shipList = testConnection.queryShip();
 			for(int i = 0; i < shipList.size(); i++) {
 				System.out.println(shipList.get(i).toString());
-		}
-		}catch(IllegalArgumentException e) {
+			}
+		} catch(IllegalArgumentException e) {
 			System.out.println("error retrieving ships");
 		}
 	}
-
 }
